6982bd62db77c4925d2ccd5eaa93dc2f76f5a491
GabihCaldeiraFlex <gabriela.caldeira@flexconsulta.com.br>
2023-09-13 17:21:00 -0300

    mudar rota para post

--- src/Routes/MaisFrete.routes.ts
+++ src/Routes/MaisFrete.routes.ts
@@ -4,7 +4,7 @@ import getInfoController from "../Controllers/MaisFrete/index.controller";
 const router = Router();
 
 const MaisFreteRoutes = () => {
-  router.get("/info", getInfoController);
+  router.post("/info", getInfoController);
 
   return router;
 };
